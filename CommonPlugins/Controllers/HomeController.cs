fc4c0c62f87c3ac7cf76b8a14860192d447f3c54
chocWan <13545689480@163.com>
2018-11-30 10:17:33 +0800

    1

---
+++ CommonPlugins/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Plugins.Models;
+
+namespace Plugins.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Table()
+        {
+            return View();
+        }
+        public IActionResult DateTime()
+        {
+            return View();
+        }
+        public IActionResult AutoAdd()
+        {
+            return View();
+        }
+        public IActionResult AutoComplete()
+        {
+            return View();
+        }
+        public IActionResult Load()
+        {
+            return View();
+        }
+        public IActionResult Progress()
+        {
+            return View();
+        }
+        public IActionResult Select()
+        {
+            return View();
+        }
+        public IActionResult StarRating()
+        {
+            return View();
+        }
+        public IActionResult Switch()
+        {
+            return View();
+        }
+        public IActionResult Tag()
+        {
+            return View();
+        }
+        public IActionResult Tree()
+        {
+            return View();
+        }
+    }
+}
--- EChart/Controllers/HomeController.cs
+++ EChart/Controllers/HomeController.cs
@@ -4,9 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Plugins.Models;
+using EChart.Models;
 
-namespace Plugins.Controllers
+namespace EChart.Controllers
 {
     public class HomeController : Controller
     {

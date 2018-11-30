0626276b9714893dbc62b11d0dcc64bc1364a44e
chocWan <13545689480@163.com>
2018-11-30 11:07:03 +0800

    2

---
+++ MVVM_Angular/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVVM_Angular.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
